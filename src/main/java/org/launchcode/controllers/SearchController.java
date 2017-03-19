fd0a8b5ae25f6ac9a378c0a7c79124b3afd6688b
Patrick Short <pjsy8c@gmail.com>
2017-03-19 11:19:54 -0500

    Add job list and search display

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static org.launchcode.controllers.ListController.columnChoices;
+
 /**
  * Created by LaunchCode
  */
@@ -18,10 +20,25 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String searchJobsByColumnAndValue(Model model,
+                                           @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+
+       // model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("columns", columnChoices);
+
+        return "search";
+    }
 
 }
